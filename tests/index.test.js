abe4b2e6a2f16b6f0390da15cc78e5eee36eb0a5
voidvoxel <160202125+voidvoxel@users.noreply.github.com>
2024-05-11 15:21:39 -0400

    Add tests

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,7 +1,49 @@
 const LimitedQueue = require('..');
 
-const queue = new LimitedQueue(3);
 
-queue.push(1, 2, 3, 4, 5);
+test(
+    "create a new `LimitedQueue`",
+    () => {
+        const queue = new LimitedQueue(3);
 
-console.log(queue.shift() === 4 && queue.shift() === 5);
+        expect(
+            queue instanceof LimitedQueue
+        ).toBe(
+            true
+        );
+    }
+);
+
+
+test(
+    "push values onto the `LimitedQueue`",
+    () => {
+        const queue = new LimitedQueue(3);
+
+        queue.push(1, 2, 3, 4, 5);
+
+        expect(queue.length).toBe(3);
+
+        expect(queue[0]).toBe(3);
+        expect(queue[1]).toBe(4);
+        expect(queue[2]).toBe(5);
+    }
+);
+
+
+test(
+    "shift the next value from a `LimitedQueue`",
+    () => {
+        const queue = new LimitedQueue(3);
+
+        queue.push(1, 2, 3, 4, 5);
+
+        const q0 = queue.shift();
+        const q1 = queue.shift();
+        const q2 = queue.shift();
+
+        expect(q0).toBe(3);
+        expect(q1).toBe(4);
+        expect(q2).toBe(5);
+    }
+);
